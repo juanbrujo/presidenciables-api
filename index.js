6b631609304808f02410023bb964bb0fba5e1a8d
Hola <hola@andalaosa.cl>
2017-04-17 16:33:03 -0300

    chore(port): change port to let Heroku decide

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000
 
 server.use(middlewares);
 server.use(router);
@@ -14,6 +15,6 @@ server.use((req, res, next) => {
   next();
 })
 
-server.listen(3000, () => {
-  console.log('JSON Server is running # localhost:3000');
+server.listen(port, () => {
+  console.log('JSON Server is running # localhost:' + port);
 });
