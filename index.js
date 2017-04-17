eb02c6b0e8798da7e63256accaadf11b73a32829
Hola <hola@andalaosa.cl>
2017-04-17 16:17:03 -0300

    initial son format

---
+++ index.js
@@ -0,0 +1,19 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.use(jsonServer.bodyParser);
+
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now();
+  }
+  next();
+})
+
+server.listen(3000, () => {
+  console.log('JSON Server is running # localhost:3000');
+});
